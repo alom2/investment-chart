b5de90d1c418491cbc2c23ae95ad00489d54f4ad
Andre Moreira <andre.lom19@gmail.com>
2020-06-13 20:29:17 -0300

    add icon on footer

--- src/features/Investments/views/investmentsChart/InvestmentsChart.js
+++ src/features/Investments/views/investmentsChart/InvestmentsChart.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faKeyboard } from '@fortawesome/free-regular-svg-icons'
 import { fetchInvestmentsData } from '../../redux'
 import { Chart, PeriodHeader } from '../../components'
 import { Card, Footer } from '../../ui'
@@ -16,7 +18,7 @@ export const InvestmentsChart = () => {
       <PeriodHeader />
       <Chart />
       <Footer>
-        Use as setas para controlar o período de exibição do gráfico
+        Use as setas para controlar o período de exibição do gráfico <FontAwesomeIcon icon={faKeyboard} />
       </Footer>
     </Card>
   )
